71878c624feb62c10d4d86909c4d5f6748c02963
kit <nikitayakuntsev@yandex.ru>
2015-07-13 14:23:08 +0300

    resteasy dependency

--- Runner/src/main/java/project/Launcher.java
+++ Runner/src/main/java/project/Launcher.java
@@ -1,11 +1,14 @@
 package project;
 
+
 import project.DAOImplementation.TestEntityService;
 import project.Entity.TestEntity;
+
 
 /**
  * Created by andrey on 13.07.15.
  */
+
 public class Launcher {
     public static void main(String[] args) {
         TestEntityService testEntityService = new TestEntityService();
