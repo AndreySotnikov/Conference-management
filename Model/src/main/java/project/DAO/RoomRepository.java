e3574114817ae9aa0e06739e1e84147aa7ea8ab5
kit <nikitayakuntsev@yandex.ru>
2015-07-13 12:35:14 +0300

    few reps added

---
+++ Model/src/main/java/project/DAO/RoomRepository.java
@@ -0,0 +1,10 @@
+package project.DAO;
+
+import project.Entity.Room;
+import project.Util.CrudRepository;
+
+/**
+ * Created by nikitayakuntsev on 13.07.15.
+ */
+public interface RoomRepository extends CrudRepository<Room, String> {
+}
