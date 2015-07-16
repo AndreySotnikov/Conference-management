04eb4b4952cadc38e74363d891cf3879664dd3c9
AndreySotnikov <100kov1994@gmail.com>
2015-07-16 16:39:25 +0400

    angular jboss

---
+++ Web/src/main/webapp/site/TestController.js
@@ -0,0 +1,37 @@
+var myModule = angular.module('app2', []);
+myModule.config(function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+//myModule.value('globalText', 'GLOBAL');
+//myModule.run(function($rootScope, globalText) {
+//    $rootScope.globalText = globalText;
+//});
+
+myModule.controller('MyCtrl', function($scope, $http) {
+    $scope.greetings = 'Hello world!';
+
+    //$http.get('http://127.0.0.1:8080/Web-1.0-SNAPSHOT/rest/all').
+    $http.get('/Web-1.0-SNAPSHOT/rest/all').
+        success(function(data) {
+            $scope.entities = data;
+        });
+});
+myModule.controller('Controller', function($scope, $http) {
+    $scope.master= {};
+    $scope.update = function(user) {
+        $scope.master= angular.copy(user);
+        $http({
+            //url: 'http://localhost:8080/Web-1.0-SNAPSHOT/rest/test',
+            url: '/Web-1.0-SNAPSHOT/rest/test',
+            method: "POST",
+            data: "name="+user.name,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        });
+    };
+    $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+    };
+    $scope.reset();
+});
+
