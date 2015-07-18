38c9bdfeab39e76de951c9a0a04994e5410652c2
Khateev Nikita <hateevnikita@yandex.ru>
2015-07-19 01:19:19 +0300

    Speaker service added

---
+++ Model/src/main/java/project/Service/SpeakerService.java
@@ -0,0 +1,35 @@
+package project.Service;
+
+import project.Entity.Speaker;
+import project.Util.CrudImplementation;
+
+import java.util.List;
+
+/**
+ * Created by nikita on 19.07.15.
+ */
+public class SpeakerService extends CrudImplementation {
+    public Speaker update (String id, Speaker speaker){
+        try {
+            Speaker oldSpeaker = findOne(Speaker.class, id);
+            oldSpeaker.setName(speaker.getName());
+            oldSpeaker.setPhone(speaker.getPhone());
+            oldSpeaker.setLogin(speaker.getLogin());
+            oldSpeaker.setEmail(speaker.getEmail());
+            return em.merge(oldSpeaker);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Speaker> findAll(){
+        return super.findAll(Speaker.class);
+    }
+
+    public Speaker findOne(String id){
+        return super.findOne(Speaker.class, id);
+    }
+
+    public void remove(String id) { super.remove(Speaker.class, id);}
+}
