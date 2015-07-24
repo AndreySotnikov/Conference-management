5d2748ebbcab7408d1bcb8ce1de7d7ad7bd1eff5
AndreySotnikov <100kov1994@gmail.com>
2015-07-24 12:28:08 +0400

    translation routes

--- Web/src/main/webapp/site/js/angularModule.js
+++ Web/src/main/webapp/site/js/angularModule.js
@@ -12,24 +12,22 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         .state('home.login', {
             url: '/login',
             templateUrl: 'views/login.html',
+            css: 'css/registration.css'
+            //controller: function($scope, $http){
+            //    
+            //}
         })
         .state('home.register', {
             url: '/register',
             templateUrl: 'views/registration.html',
+            css: 'css/registration.css',
             controller: function($scope, $http) {
                 $scope.master= {};
                 $scope.clickBtn = function (user) {
                     $scope.master= angular.copy(user);
                     $http({
-                        //url: 'http://127.0.0.1:8080/Web-1.0-SNAPSHOT/rest/register',
                         url: '/Web-1.0-SNAPSHOT/rest/register',
                         method: "POST",
-                        //data: { 'login' : user.username,
-                        //    'password' : user.password,
-                        //    'role' : user.role,
-                        //    'phone' : user.phone,
-                        //    'email' : user.email,
-                        //    'name' : user.name},
                         data: "login="+user.username+
                             "&password="+user.password+
                             "&role="+user.role+
@@ -41,9 +39,83 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
                 }
 
             }
+        })
+        .state('translation', {
+            url: '/translation',
+            templateUrl: 'views/translation.html',
+            controller: 'translationCtrl'
         });
 });
+routerApp.controller('translationCtrl', function($scope, $http) {
 
+    $scope.period = 30;
+    $scope.defaultperiod = 30;
+
+    $('#showup').hide();
+
+    $scope.reports = [
+        "time",
+        "text"
+    ];
+    $scope.reports = [];
+
+    $scope.warName = "Web-1.0-SNAPSHOT";
+    $scope.speechId = 2;
+    $scope.lastId = 0;
+    $scope.server = "http://localhost:8080/";
+    // ������� ��������� �������
+    $http.get($scope.server + $scope.warName + "/rest/speech/topic/" + $scope.speechId)
+        .success(function (data) {
+            $scope.title = data;
+        });
+    // ������� ��� ��������� ����������
+    $http.get($scope.server + $scope.warName + "/rest/trans/fbs/" + $scope.speechId)
+        .success(function (data) {
+            angular.forEach(data, function(elem) {
+                $scope.reports.push({time:elem[2], text:elem[1]});
+            });
+            if (data.length>0) {
+                $scope.lastId = data[data.length - 1][0];
+            }
+        });
+
+    $scope.autoUpdates = function() {
+        if ($scope.autoUpdate){
+            $scope.more();
+            if ($scope.myForm.$valid){
+                setTimeout($scope.autoUpdates, 1000*$scope.period);
+            }else{
+                setTimeout($scope.autoUpdates, 1000*$scope.defaultperiod);
+            }
+
+        }
+    }
+
+    // �������� ����������
+    $scope.more = function() {
+        $http({
+            url: $scope.server + $scope.warName + "/rest/trans/updates",
+            method: "GET",
+            params: {speechId: $scope.speechId, lastId: $scope.lastId}
+        }).success(function (data) {
+            angular.forEach(data, function(elem) {
+                $scope.reports.push({time:elem[2], text:elem[1]});
+            });
+            if (data.length>0) {
+                $scope.lastId = data[data.length - 1][0];
+            }
+        });
+    }
+
+    $scope.changeCheckbox = function() {
+        if ($scope.autoUpdate){
+            $('#showup').show();
+            $scope.autoUpdates();
+        }else{
+            $('#showup').hide();
+        }
+    }
+});
 
 
 
